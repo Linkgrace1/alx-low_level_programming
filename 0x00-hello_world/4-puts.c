3068d63b70d03f53885ff3b83ea8c239e08f77e0
Linkgrace1 <ifyflourish12@gmail.com>
2023-08-11 02:25:42 -0700

    Hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 /**
- * main - Entry Point 
- *               
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
  * Return: Always 0.
  */
 int main(void)
