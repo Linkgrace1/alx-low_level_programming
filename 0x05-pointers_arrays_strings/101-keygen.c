c8cb12f881d5cc4cc389ff21bbc4681f301e0592
Linkgrace1 <ifyflourish12@gmail.com>
2023-08-21 13:09:59 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - program that generates random valid
  * passwords for the program 101-crackme
@@ -29,5 +30,6 @@ int main(void)
 			break;
 		}
 	}
+
 	return (0);
 }
